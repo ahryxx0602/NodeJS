165be5c26400dbdf9078450c8aea7e53db7613d8
Ahryxx <phvanthanh06@gmail.com>
2025-03-09 10:44:08 +0700

    NodeJS_APIS_Login

--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
 
 
 let route = express.Router();
@@ -15,6 +16,7 @@ let initWebRoutes = (app) => {
     route.post("/put-crud", homeController.putCRUD);
     route.get("/delete-crud", homeController.deleteCRUD);
 
+    route.post("/api/login", userController.handleLogin);
 
     return app.use("/", route);
 }
