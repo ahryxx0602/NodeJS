d6c3edb9b9e9a45f58e6e87760c29111bdd8b80f
Ahryxx <phvanthanh06@gmail.com>
2025-03-27 21:34:53 +0700

    NodeJS_ForAdmin_1st

--- src/route/web.js
+++ src/route/web.js
@@ -22,6 +22,8 @@ let initWebRoutes = (app) => {
     route.put('/api/edit-user', userController.handleEditUser);
     route.delete('/api/delete-user', userController.handleDeleteUser);
 
+    route.get('/allcode', userController.getAllCode);
+
     return app.use("/", route);
 }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -108,24 +108,26 @@ let createNewUser = (data) => {
             if (check === true) {
                 resolve({
                     errCode: 1,
-                    message: 'Your email is already in use, Pls try another'
+                    errMessage: 'Your email is already in use, Pls try another'
                 })
+            } else {
+                let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+                await db.User.create({
+                    email: data.email,
+                    password: hashPasswordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phoneNumber: data.phoneNumber,
+                    gender: data.gender === "1" ? true : false,
+                    roleId: data.roleId
+                })
+                resolve({
+                    errCode: 0,
+                    message: 'OK'
+                });
             }
-            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-            await db.User.create({
-                email: data.email,
-                password: hashPasswordFromBcrypt,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                address: data.address,
-                phoneNumber: data.phoneNumber,
-                gender: data.gender === "1" ? true : false,
-                roleId: data.roleId
-            })
-            resolve({
-                errCode: 0,
-                message: 'OK'
-            });
+
         } catch (e) {
             reject(e);
         }
@@ -187,14 +189,37 @@ let updateUserData = (data) => {
     })
 }
 
+let getAllCodeServices = (typeInput) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!typeInput) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameters!'
+                })
+            } else {
+                let res = {};
+                let allcode = await db.allcode.findAll({
+                    where: { type: typeInput }
+                });
+                res.errCode = 0;
+                res.data = allcode;
+                resolve(res);
+            }
+
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
 module.exports = {
     handleUserLogin: handleUserLogin,
     checkUserEmail: checkUserEmail,
     getAllUsers: getAllUsers,
     createNewUser: createNewUser,
     deleteUser: deleteUser,
-    updateUserData: updateUserData
-
-
+    updateUserData: updateUserData,
+    getAllCodeServices, getAllCodeServices,
 
 }
